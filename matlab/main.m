c519cb610c9c758b7e438ad89d6d9c43dad04bc1
Jonas Wallin <jonas.wallin81@gmail.com>
2015-11-20 14:00:34 +0100

    added matlab v2

--- matlab/main.m
+++ matlab/main.m
@@ -16,6 +16,7 @@ obj.X = cell(N, 1);
 obj.Y = cell(N, 1);
 
 for i = 1:N
+    obj.evalX{i} = load(['../data/ERA_',num2str(i),'.dat']);
     obj.X{i} = load(['../data/ERA_',num2str(i),'.dat']);
     obj.Y{i} = load(['../data/Yts_',num2str(i),'.dat']);
 end
@@ -35,6 +36,7 @@ f_1 = @(x) likelihood_1(x, obj);
  beta_1  = posterior_beta_1(theta_1, obj);
 
  predicit = cell(N,4);
+ options = optimset('MaxFunEvals',10000);
  for j = 1:N
     obj_crossval = obj;
     obj_crossval.X = cell(N-1, 1);
@@ -54,7 +56,12 @@ f_1 = @(x) likelihood_1(x, obj);
     beta_1  = posterior_beta_1(theta_1, obj_crossval);
     
     predicit{j, 1} = beta;
-    predicit{j, 2} = exp(obj.Y{j}) - exp(beta.*obj.X{j} + theta(3)) ;
+    predicit{j, 2} = exp(obj.Y{j}) - exp(beta.*obj.evalX{j} + theta(3)) ;
     predicit{j, 3} = beta_1;
-    predicit{j, 4} = exp(obj.Y{j}) - exp(beta_1.*obj.X{j} + theta_1(2)) ;
+    predicit{j, 4} = exp(obj.Y{j}) - exp(beta_1.*obj.evalX{j} + theta_1(2)) ;
+    
+ end
+ for j=1:N
+     fprintf('mse_%d  = %.2f\n',j,mean(predicit{j, 2}.^2 ));
+     
  end
